f61500cb2f73edecaff61e9411a8c26a50849586
Tilak <adireddytilak@gmail.com>
2025-10-30 15:29:42 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import Form from './Form.js'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Form from './Form';
+import FormRef from './FormRef';
+import FormState from './FormState';
+
 const App = () => {
   return (
-    <>
-    <div>App</div>
-    <Form/>
-    </>
-  )
-}
+    <Router>
+      <div>
+        <h1>React Form Handling</h1>
+        <nav>
+          <ul>
+            <li><Link id="form-link" to="/form">Form Layout</Link></li>
+            <li><Link id="form-ref-link" to="/form-ref">Form with useRef</Link></li>
+            <li><Link id="form-state-link" to="/form-state">Form with useState</Link></li>
+          </ul>
+        </nav>
 
-export default App
+        <Routes>
+          <Route path="/form" element={<Form />} />
+          <Route path="/form-ref" element={<FormRef />} />
+          <Route path="/form-state" element={<FormState />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
